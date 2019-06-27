ef70f28a05179bb877d2fac3c090215ef084b05d
rguastavino <guastavino.renzo@hotmail.com>
2019-06-27 18:02:59 -0300

    Added Intro Scene Animation with Sound

---
+++ Source/VREL/Assets/Scripts/IntroAnimationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroAnimationController : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void IntroStart(){
+        audioSource.Play();
+    }
+
+    public void IntroEnd(){
+        SceneManager.LoadScene("VREL_MainMenuScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
